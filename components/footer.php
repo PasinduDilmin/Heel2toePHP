c35085c0bf3f36e0d0890bc883d11c91a8945cf3
Pasindu Dilmin Weerasinghe <90268451+PasinduDilmin@users.noreply.github.com>
2025-03-18 11:27:23 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+
+   <section class="grid">
+       <table><tr><td>
+	   <div class="box">
+	     <a href="home.php" class="logo"><b>Heel 2 Toe</b><span>.</span></a>
+        <p>Discover our extensive range of chic<br> and comfortable footwear crafted to <br> enhance your every step. With <br>premium materials and meticulous <br>craftsmanship, our shoes<br> offer unmatched durability,<br> quality, and elegance.</p>
+      </div>
+
+      </td><td>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+      </td><td>
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> 076-8088101</a>
+         <a href="tel:11122233333"><i class="fas fa-phone"></i>071-1256289</a>
+         <a href="mailto:Heel2toe@gmail.com"><i class="fas fa-envelope"></i> Heel2toe@gmail.com</a>
+         <a href="https://earth.google.com/web/@6.784626,81.101074,343.95718377a,1000d,30y,0h,0t,0r/data=MiQKIgogChwxV25SYXNiVzZIaGpLaTFfOTE2MFF6N09ZMUZZIAI6AwoBMA"><i class="fas fa-map-marker-alt"></i> Bandarawela, Sri Lanka - 90100 </a>
+      </div>
+        </td><td>
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-youtube"></i>Youtube</a>
+      </div>
+        </td></tr></table>
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Group 13 - COST 22073</span> | all rights reserved!</div>
+
+</footer>
